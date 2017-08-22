8f42492470349a222acd9dccdda473f1499e3bc3
Mark Old <dlom234@gmail.com>
2017-08-22 13:14:18 -0700

    Change indentation to spaces (how was it not?)

--- src/EncounterBot.ts
+++ src/EncounterBot.ts
@@ -3,32 +3,32 @@ import { Snowflake } from "discord.js";
 import * as path from "path"
 
 export class EncounterBot {
-	private client: CommandoClient;
+    private client: CommandoClient;
 
     constructor(private token: string, owner: string) {
         this.client = new CommandoClient({ owner });
         this.client
-			.on("error", console.error)
-			.on("warn", console.warn)
-			.on("debug", console.log)
-			.on("ready", () => {
-				console.log(`Logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id})`);
-			})
-			.on("disconnect", () => { console.warn("Disconnected!"); })
-			.on("reconnect", () => { console.warn("Reconnecting..."); })
-			.on("commandError", (cmd, err) => {
-				if (err instanceof FriendlyError) return;
-				console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-			});
+            .on("error", console.error)
+            .on("warn", console.warn)
+            .on("debug", console.log)
+            .on("ready", () => {
+                console.log(`Logged in as ${this.client.user.username}#${this.client.user.discriminator} (${this.client.user.id})`);
+            })
+            .on("disconnect", () => { console.warn("Disconnected!"); })
+            .on("reconnect", () => { console.warn("Reconnecting..."); })
+            .on("commandError", (cmd, err) => {
+                if (err instanceof FriendlyError) return;
+                console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+            });
 
-		this.client.registry
-			.registerDefaults()
-		    .registerGroups([
-				["encounter", "Encounter"],
-				["player", "Player"]
-			])
-			.registerCommandsIn(path.join(__dirname, "Commands"));
-	}
+        this.client.registry
+            .registerDefaults()
+            .registerGroups([
+                ["encounter", "Encounter"],
+                ["player", "Player"]
+            ])
+            .registerCommandsIn(path.join(__dirname, "Commands"));
+    }
 
     public async start() {
         console.log("Starting EncounterBot...");
